5db1922c38443ac7d339009fa5bc07bb66e9ecd8
Piotr Kotulski <piotr.kotulski1986@gmail.com>
2023-07-15 00:12:52 +0200

    added founctionality of making article enable/disable

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const titleClickHandler = function(event){
 
+    event.preventDefault();
+    const clickedElement = this;
+
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-    console.log(activeLinks);
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active')
     }
 
     /* add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post.active');
@@ -18,10 +21,16 @@ const titleClickHandler = function(event){
     }
 
     /* get 'href' attribute from the clicked link */
+    const activeLink = document.querySelector('.titles a.active');
+    const hrefToArticle = activeLink.getAttribute('href');
+
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const articleToShow = document.querySelector(hrefToArticle);
+
 
     /* add class 'active' to the correct article */
+    articleToShow.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
