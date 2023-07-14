9b2b498be02df5856ef09839b214e7464dbd4c5d
Piotr Kotulski <piotr.kotulski1986@gmail.com>
2023-07-14 23:13:34 +0200

    added function removing active classes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event.target);
 
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    console.log(activeLinks);
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active')
+    }
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
 
